4a1ff46fb652b13045e999d17d955b065601b448
Artemiy Gudz <mercartem@mail.ru>
2022-12-08 13:13:58 +0300

    fix: change baselink to proxy

--- news-js/src/components/controller/appLoader.ts
+++ news-js/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://newsapi-redirect-production.up.railway.app/', {
       apiKey: '8cd53810f57a41c6951fe15558e103f6', // получите свой ключ https://newsapi.org/
     });
   }
